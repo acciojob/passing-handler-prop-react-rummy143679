e10753777512c75811d9ef56a37c395a14c9ab54
matteda ramesh <mattedaramesh27@gmail.com>
2025-07-05 13:15:18 +0530

    inital commit

--- src/components/App.js
+++ src/components/App.js
@@ -4,21 +4,21 @@ import Selection from './Selection';
 import ColourSelector from './ColourSelector';
 
 const colourConfig = [{
-    key: 'blue',
-    label: 'Blue',
-    classname: 'btn-blue',
-    background: 'rgb(34, 193, 195)'
-  }, {
-    key: 'orange',
-    label: 'Orange',
-    classname: 'btn-orange',
-    background: 'rgb(221, 112, 18)'
-  }, {
-    key: 'green',
-    label: 'Green',
-    classname: 'btn-green',
-    background: 'rgb(44, 209, 88)'
-  }
+  key: 'blue',
+  label: 'Blue',
+  className: 'btn-blue',
+  background: 'rgb(34, 193, 195)'
+}, {
+  key: 'orange',
+  label: 'Orange',
+  className: 'btn-orange',
+  background: 'rgb(221, 112, 18)'
+}, {
+  key: 'green',
+  label: 'Green',
+  className: 'btn-green',
+  background: 'rgb(44, 209, 88)'
+}
 ]
 
 const title = 'Select the gradient and then the Box to change the color';
@@ -31,18 +31,17 @@ const App = () => {
 
   return (
     <div id="master">
-      <h5 className="heading">{/* display title here */}</h5>
-
-      <div className="row">
+      <h5 className="heading">{title}</h5>
+      <div className="colour-selector">
         {colourConfig.map((config, index) => (
           <ColourSelector key={config.key} config={config} selectNextBackground={selectNextBackground} />
         ))}
       </div>
 
-      <div className='row' id="children-wrapper">
+      <div className='holder' id="children-wrapper">
         {
-          ["selection1", "selection2", "selection3"].map(key => (
-            <Selection key={key} applyColor={applyColor} />
+          ["selection1", "selection2", "selection3"].map((item, key) => (
+            <Selection key={key} applyColor={applyColor} boxTitle={item} nextBackground={nextBackground} />
           ))
         }
       </div>
--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,17 @@
 
+import React, { useState } from 'react'
+import '../styles/Child.css';
+
+export default function Selection(props) {
+    const { applyColor, boxTitle } = props;
+    const [background, setBackground] = useState("");
+
+    const updateSelectionStyle = (colorObj) => {
+        setBackground(colorObj.background);
+    };
+    return (
+        <div className="fix-box" onClick={() => applyColor(updateSelectionStyle)} style={{ backgroundColor: background }}>
+            <p className="subheading">{boxTitle}</p>
+        </div>
+    )
+}
